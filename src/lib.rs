0b625ac9278cfa8695deea74e0b2611564bef6c6
Xiphoseer <daniel.g.seiler+lugh@gmail.com>
2020-03-30 23:48:43 +0200

    Add some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! # Paradox - LU toolkit
+//!
+//! This is the `paradox` CLI toolkit exposed as a library. It is a collection of scripts which
+//! may be used to read, convert or manipulate data from the defunct game "LEGO (R) Universe".
+//!
+//! ## Changelog
+//!
+//! ### 2020-03-30
+//! - `wrench`: Completed JSON export of scripts (LUA files)
+//!
+//! ## TODO
+//!
+//! - Add maps to `paradox wrench`
+//! - Add objects and behaviors to `wrench` or update the explorer
+//! - Implement `refs`
 use structopt::StructOpt;
 
 pub mod util;
